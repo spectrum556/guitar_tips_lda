cfbfb7dc95d26eeb303f5adc7894cae87651ddc7
ihor <fesianov.i@gmail.com>
2017-09-27 01:53:03 +0300

    Add class Similar

---
+++ LDA_similarities.py
@@ -0,0 +1,42 @@
+from gensim import corpora, models, similarities
+from pprint import pprint
+from constants import *
+
+
+class Similar:
+    def __init__(self):
+        self.corpus = corpora.MmCorpus(CORPUS_FILENAME)
+        self.dictionary = corpora.Dictionary.load(DICT_FILENAME)
+        self.lda = lda = models.LdaModel.load(LDA_FILENAME)
+        try:
+            self.index = similarities.MatrixSimilarity.load(INDEX_FILENAME)
+        except FileNotFoundError:
+            self.index = self._make_index()
+
+    def _make_index(self):
+        """
+        transform corpus to LDA space and index it
+        """
+        index = similarities.MatrixSimilarity(self.lda[self.corpus])
+        index.save(INDEX_FILENAME)
+
+    def get_similar_doc_numbers(self, number_in_dict, probability_threshold):
+        """
+        :return list of tuples (number of doc, similarity probability) for docs with
+        probability higher probability_threshold
+        """
+        # convert the query to LDA space
+        vec_lda = self.lda[self.corpus[number_in_dict]]
+
+        # perform a similarity query against the corpus
+        sims = self.index[vec_lda]
+        sort_sims = sorted(enumerate(sims), key=lambda item: -item[1])
+        return self._apply_threshold(sort_sims, probability_threshold)
+
+    def _apply_threshold(self, sims, threshold):
+        return list(filter(lambda item: item[1] >= threshold, sims))
+
+
+if __name__ == '__main__':
+    sims = Similar()
+    print(sims.get_similar_doc_numbers(18, 0.95))
--- LDA_vis.py
+++ LDA_vis.py
@@ -9,5 +9,7 @@ lda = models.LdaModel.load(LDA_FILENAME)
 
 pprint(lda.print_topics())
 vis_data = pyLDAvis.gensim.prepare(lda, corpus, dictionary)
-# pyLDAvis.display(vis_data)
 pyLDAvis.save_html(vis_data, VIS_FILENAME)
+
+
+# pyLDAvis.display(vis_data)
